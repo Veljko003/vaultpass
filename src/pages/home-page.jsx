5516d72fbbe725cb829720f2123418672dad9808
TheVSDev <vspasic03@gmail.com>
2023-08-05 21:44:39 +0200

    update: Title and Container components

---
+++ src/pages/home-page.jsx
@@ -0,0 +1,13 @@
+// Imports
+import Title from "@/web/components/Title"
+
+// HomePage function
+const HomePage = () => {
+    return (
+        <>
+            <Title titleLabel="VaultPass" />
+        </>
+    )
+}
+
+export default HomePage
