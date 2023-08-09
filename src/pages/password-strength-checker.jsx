633cb2721d4773f0d769d103cd816a0cf6fd0ad1
TheVSDev <vspasic03@gmail.com>
2023-08-09 23:04:16 +0200

    filler

--- src/pages/password-strength-checker.jsx
+++ src/pages/password-strength-checker.jsx
@@ -1,22 +1,23 @@
 // Imports
-import HeadPage from "@/web/components/HeadPage"
-import Main from "@/web/components/Main"
-import Footer from "@/web/components/Footer"
-import Sidebar from "@/web/components/Sidebar"
-import ThemeSwitchButton from "@/web/components/ThemeSwitchButton"
+import HeadPage from "@/web/components/HeadPage";
+import Main from "@/web/components/Main";
+import Footer from "@/web/components/Footer";
+import Sidebar from "@/web/components/Sidebar";
+import ThemeSwitchButton from "@/web/components/ThemeSwitchButton";
+import styles from "@/web/styles/Tools.module.css";
 
 // PasswordStrengthChecker function
 const PasswordStrengthChecker = () => {
-    return (
-        <>
-            <HeadPage />
-            <Main>
-                <ThemeSwitchButton />
-                <Sidebar />
-            </Main>
-            <Footer />
-        </>
-    )
-}
+  return (
+    <>
+      <HeadPage />
+      <Main>
+        <ThemeSwitchButton />
+        <Sidebar />
+      </Main>
+      <Footer />
+    </>
+  );
+};
 
-export default PasswordStrengthChecker
+export default PasswordStrengthChecker;
