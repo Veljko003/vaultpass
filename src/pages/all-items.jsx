29e68038022a85ffe0aee76ee01424700c2b1316
TheVSDev <vspasic03@gmail.com>
2023-08-16 01:41:58 +0200

    update: add item button icons

--- src/pages/all-items.jsx
+++ src/pages/all-items.jsx
@@ -1,6 +1,7 @@
 // Imports
 import React, { useState } from "react";
 import Link from "next/link"
+import Image from "next/image"
 
 import HeadPage from "@/web/components/HeadPage";
 import Main from "@/web/components/Main";
@@ -26,10 +27,10 @@ const AllItems = () => {
         <Button onClick={toggleDropdown} btnLabel="Add item" />
         {isDropdownVisible && (
           <div className={`${styles.dropdown} ${styles.visible}`}>
-            <Link href="/formPages/passwordForm"><span className={styles.item}>Password</span></Link><br /><br />
-            <Link href="/formPages/creditCardForm"><span className={styles.item}>Credit Card</span></Link><br /><br />
-            <Link href="/formPages/secureNoteForm"><span className={styles.item}>Secure Note</span></Link><br /><br />
-            <Link href="/formPages/personalInfoForm"><span className={styles.item}>Personal Info</span></Link>
+            <Link href="/formPages/passwordForm"><span className={styles.item}><Image src="/password.svg" alt="" width={20} height={10} className={styles.icon} />Password</span></Link><br /><br />
+            <Link href="/formPages/creditCardForm"><span className={styles.item}><Image src="/credit-card.svg" alt="" width={20} height={10} className={styles.icon} />Credit Card</span></Link><br /><br />
+            <Link href="/formPages/secureNoteForm"><span className={styles.item}><Image src="/secure-note.svg" alt="" width={20} height={10} className={styles.icon} />Secure Note</span></Link><br /><br />
+            <Link href="/formPages/personalInfoForm"><span className={styles.item}><Image src="/personal-info.svg" alt="" width={20} height={10} className={styles.icon} />Personal Info</span></Link>
           </div>
         )}
       </Main>
