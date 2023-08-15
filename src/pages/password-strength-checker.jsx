8f3fc2bb52c5b0897e8d549064bc12b4e1907ff2
TheVSDev <vspasic03@gmail.com>
2023-08-15 13:12:42 +0200

    done: password strength checker

--- src/pages/password-strength-checker.jsx
+++ src/pages/password-strength-checker.jsx
@@ -1,19 +1,56 @@
 // Imports
+import React, { useState } from "react";
+import zxcvbn from "zxcvbn";
+
 import HeadPage from "@/web/components/HeadPage";
 import Main from "@/web/components/Main";
 import Footer from "@/web/components/Footer";
 import Sidebar from "@/web/components/Sidebar";
 import ThemeSwitchButton from "@/web/components/ThemeSwitchButton";
 import styles from "@/web/styles/Tools.module.css";
+import "@/web/services/serviceWorker.js";
 
 // PasswordStrengthChecker function
 const PasswordStrengthChecker = () => {
+  const [type, setType] = useState("input");
+  const [score, setScore] = useState("null");
+
+  const showHide = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    let currentType = type === "input" ? "password" : "input";
+    setType(currentType);
+  };
+
+  const testStrengthPassword = (e) => {
+    // we will get score property from zxcvbn
+    if (e.target.value !== "") {
+      let pass = zxcvbn(e.target.value);
+      setScore(pass.score);
+    } else {
+      setScore("null");
+    }
+  };
   return (
     <>
       <HeadPage />
       <Main>
         <ThemeSwitchButton />
         <Sidebar />
+        <div className={styles.container}>
+          <label className={styles.labelPassword}>
+            Password
+            <input
+              type={type}
+              className={styles.inputPassword}
+              onChange={testStrengthPassword}
+            />
+            <span className={styles.showPassword} onClick={showHide}>
+              {type === "input" ? "Hide" : "Show"}
+            </span>
+            <span className={styles.strengthPassword} data-score={score} />
+          </label>
+        </div>
       </Main>
       <Footer />
     </>
